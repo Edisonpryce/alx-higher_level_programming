90b6a3bd48363e49679bd14ad95f3b329ac7b5f0
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2022-08-29 23:43:29 +0000

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total = len(sys.argv)
-    if total <= 1:
-        print("0 argument.")
-    else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    lst = sys.argv
+    lth = len(lst) - 1
+    arg = "argument"
+    print("{} argument{}{}".format(
+            lth,
+            "" if lth == 1 else "s",
+            "." if lth == 0 else ":"))
+    for i in range(1, len(lst)):
+        print("{}: {}".format(i, lst[i]))
