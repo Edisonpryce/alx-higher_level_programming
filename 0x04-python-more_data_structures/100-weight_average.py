54c27a24caa610db6b853a417fa293b8264203c7
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2022-09-01 22:28:56 +0000

    updated

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) is 0:
+    div = 0
+    add_mul = 0
+
+    if not my_list:
         return 0
-    a = list(map(list, zip(*my_list)))
-    b = [x * y for x, y in zip(a[0], a[1])]
-    return sum(b) / sum(a[1])
+
+    for a, b in my_list:
+        mul = 0
+        mul = a * b
+        add_mul += mul
+        div += b
+
+    average = add_mul / div
+    return average
