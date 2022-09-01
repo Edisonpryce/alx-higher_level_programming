2c00737892c68e44c0509d9af6c24131848b5d00
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2022-09-01 22:30:52 +0000

    102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def complex_delete(my_dict, value):
-    copy = my_dict.copy()
-    for k, v in copy.items():
-        if value in v:
-            del my_dict[k]
-    return my_dict
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary.keys()):
+        if a_dictionary[i] == value:
+            del(a_dictionary[i])
+    return a_dictionary
