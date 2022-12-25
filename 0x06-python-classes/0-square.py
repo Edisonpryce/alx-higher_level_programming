0097d080b283610d591b53e29dd577c1ea2844a7
Edison Asare <edisonpryce@gmail.com>
2022-12-25 04:04:55 +0000

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+'''
+This is a small documentation for this task.
+Definding an empty class and assign nothing to it is the purpose of this task
+I hope this documentation will help settle what is rejected
+'''
 class Square:
     pass
