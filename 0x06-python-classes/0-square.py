69c4fcfa5188255634746f0a9ae13579c46cd58e
Edison Asare <edisonpryce@gmail.com>
2022-12-25 04:08:52 +0000

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,5 +4,7 @@ This is a small documentation for this task.
 Definding an empty class and assign nothing to it is the purpose of this task
 I hope this documentation will help settle what is rejected
 '''
+
 class Square:
+    '''Does nothing'''
     pass
