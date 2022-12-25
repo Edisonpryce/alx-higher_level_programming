b8b1a3eab6af3e8fe9d7c3318e6bb3d8f941f13a
Edison Asare <edisonpryce@gmail.com>
2022-12-25 04:30:21 +0000

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,6 +9,6 @@ class Square:
     def __init__(self, size):
         '''Initialiazing this square class
         Args: size - represents the size of the square defined
-        ''
+        '''
 
         self.__size = size
