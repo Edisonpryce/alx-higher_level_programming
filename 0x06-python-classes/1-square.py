efa6cbaec09babf4265d852dee30b302f4eb7041
Edison Asare <edisonpryce@gmail.com>
2022-12-25 04:24:15 +0000

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py by Edison 
+'''Define class square'''
+
+
+class Square:
+    '''Defining a private instance'''
+
+    def __init__(self, size):
+        '''Initialiazing this square class
+        Args: size - represents the size of the square defined
+        ''
+
+        self.size = size
