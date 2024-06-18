68e2af3811f3e6a97e8e3f0e70b285e5035ba669
edison pryce <edisonpryce@gmail.com>
2024-06-18 16:34:39 -0700

    new

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Script to print string 'Javascript is amazing'
+
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
 !#/usr/bin/node
-//scrip that prints 3 lines
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+//scrip that prints 3 linei
+console.log('C is fun\nPython is cool\nJavascript is amazing');
