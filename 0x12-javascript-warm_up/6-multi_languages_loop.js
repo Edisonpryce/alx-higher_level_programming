4ca6d3a0fafac3cd53a1e116185d72393e8f60dc
edison pryce <edisonpryce@gmail.com>
2024-06-20 14:15:43 -0700

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js) using array
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let x = 0; x < myArray.length; x++) {
-  console.log(myArray[x]);
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
