96aa51e32957d8a012299d7d4d6dd5724f5a9e8b
edison pryce <edisonpryce@gmail.com>
2024-06-21 12:01:12 -0700

    new

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ def main():
           host="localhost",
           port=3306,
           password=sys.argv[2],
-          db=sys.argv[3],
+          database=sys.argv[3]
     )
     mycursor = db.cursor()
     mycursor.execute(
