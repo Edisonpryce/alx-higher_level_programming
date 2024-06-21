ee80f449d5dd34c0cb78cde18687e6db5b2199c9
edison pryce <edisonpryce@gmail.com>
2024-06-21 11:54:43 -0700

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-"""lists states by user name"""
+""" Script to protect against sql injection"""
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
-    mycursor = conn.cursor()
-    mycursor.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-                (sys.argv[4], ))
+
+def main():
+    db = MySQLdb.connect(
+          user=sys.argv[1],
+          host="localhost",
+          port=3306,
+          password=sys.argv[2],
+          db=sys.argv[3],
+    )
+    mycursor = db.cursor()
+    mycursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
     results = mycursor.fetchall()
     for row in results:
         print(row)
     mycursor.close()
     db.close()
+
+
+ 
+
+if __name__ == "__main__":
+    main()
