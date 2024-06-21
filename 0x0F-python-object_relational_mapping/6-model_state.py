aaae21f25c2d31d2071e46965f8a7e658f5ee954
edison pryce <edisonpryce@gmail.com>
2024-06-21 16:19:32 -0700

    new

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-"""
-State and clss instance define
-"""
 
-from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
 
+
+# Create an instance of declarative_base
 Base = declarative_base()
 
 
 class State(Base):
-    """ State represemtation for table """
-    __tablename__ = 'state'
-    id = Colunm(Interger, autoincrement=True, primary_key=True)
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+def database_engine():
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+            )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.close()
+
+
+if __name__ == "__main__":
+    database_engine()
