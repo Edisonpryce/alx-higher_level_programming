609c668edcf73d13197206988702bfa268f56716
edison pryce <edisonpryce@gmail.com>
2024-06-21 09:57:11 -0700

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,8 +4,15 @@
 import MySQLdb
 import sys
 
+
 def main():
-    db = MySQLdb.connect(user=sys.argv[1], host="localhost", port=3306, password=sys.argv[2], db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(
+          user=sys.argv[1],
+          host="localhost",
+          port=3306,
+          password=sys.argv[2],
+          db=sys.argv[3]
+          )
     mycursor = db.cursor()
     mycursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     results = mycursor.fetchall()
@@ -15,6 +22,6 @@ def main():
     mycursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     main()
-
