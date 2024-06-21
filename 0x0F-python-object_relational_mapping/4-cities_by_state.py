984413da7a4947d695988db90562ada7f9a16eb4
edison pryce <edisonpryce@gmail.com>
2024-06-21 12:09:47 -0700

    new

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script listing cities by states"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+          user=sys.argv[1],
+          host="localhost",
+          port=3306,
+          password=sys.argv[2],
+          database=sys.argv[3]
+    )
+    mycursor = db.cursor()
+    mycursor.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    results = mycursor.fetchall()
+    for row in results:
+        print(row)
+    mycursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
