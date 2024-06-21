39f821fa053f0e1703a09a595251013962fecf71
edison pryce <edisonpryce@gmail.com>
2024-06-21 11:20:12 -0700

    update

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" list states by user input"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+          user=sys.argv[1],
+          host="localhost",
+          port=3306,
+          password=sys.argv[2],
+          db=sys.argv[3],
+    )
+    mycursor = db.cursor()
+    mycursor.execute("""
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC""".format(sys.argv[4]))
+    results = mycursor.fetchall()
+    for row in results:
+        print(row)
+    mycursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
