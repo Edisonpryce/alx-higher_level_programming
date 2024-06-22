7d53c6961e5e9b1607072f5cc3b752ad9795a4e0
edison pryce <edisonpryce@gmail.com>
2024-06-22 09:10:00 -0700

    all

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -8,16 +8,21 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
+def db_engine():
     user = sys.argv[1]
     password = sys.argv[2]
     db = sys.argv[3]
 
     engine = create_engine(
-            f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
-            )
+        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+        )
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
+    states = session.query(State).all()
     session.close()
+
+
+if __name__ == "__main__":
+    db_engine()
