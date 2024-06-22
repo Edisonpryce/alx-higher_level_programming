11de5637212733df72d558e6959255a31c44ca82
edison pryce <edisonpryce@gmail.com>
2024-06-22 02:06:20 -0700

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+''' Script creates first sqlalchemy state model '''
 
-
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import sys
 
 
 # Create an instance of declarative_base
@@ -16,24 +15,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-def database_engine():
-
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db = sys.argv[3]
-
-    engine = create_engine(
-            f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
-            )
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    session.close()
-
-
-if __name__ == "__main__":
-    database_engine()
