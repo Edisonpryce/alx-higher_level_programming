5249c0d04e1087667cc2d711ee4ef7650376c536
edison pryce <edisonpryce@gmail.com>
2024-06-22 02:15:02 -0700

    updated

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """Script connects to mysql db """
 
+
 from sqlalchemy import create_engine
 import sys
 from model_state import Base, State
 
+
 def db_engine():
     user = sys.argv[1]
     password = sys.argv[2]
-    db= sys.argv[3]
+    db = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}')
+    engine = create_engine(
+            f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+            )
     Base.metadata.create_all(engine)
 
+
 if __name__ == "__main__":
-    db_engine()    
+    db_engine()
