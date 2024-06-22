6a4134a72d3de9758672d95d0a23a1a786c2b6ca
Edison Asare <104757179+Edisonpryce@users.noreply.github.com>
2024-06-22 18:04:17 +0000

    Update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,6 +6,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
 
 class City(Base):
+    """ City class
+    __tablename__: its thetable of the class City
+    id (int): the id of the class City
+    name (str): the name of the class City
+    state_id (int): Its the id of the state of the class City
+    """
+    
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
