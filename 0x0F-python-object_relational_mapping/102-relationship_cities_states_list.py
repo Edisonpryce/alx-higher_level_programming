ed2592de655c3a269918327327ca0cecc55b0648
edison pryce <edisonpryce@gmail.com>
2024-06-24 01:21:52 -0700

    update

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,7 +4,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from relationship_state import Base, State
 from relationship_city import City
 import sys
 
