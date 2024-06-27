bf81fb4942f96e4a4a1c8fc129c529b4b585ed7f
edisonpryce <edisonpryce@gmail.com>
2024-06-27 15:48:41 -0700

    update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -13,7 +13,6 @@ class City(Base):
     name (str): the name of the class City
     state_id (int): Its the id of the state of the class City
     """
-    
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
