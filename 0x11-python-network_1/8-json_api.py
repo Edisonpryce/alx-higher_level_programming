95436b4fcf2e5bf0f6050fb0b2b9464442e274d5
Edison pryce <Edisonpryce@gmail.com>
2024-07-05 19:13:50 -0700

    new

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""POST request sent to http://0.0.0.0:5000/search_user with a letter.
+Usage(cmd): ./8-json_api.py <letter>
+"""
+import sys
+import requests
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__:
+    main()
