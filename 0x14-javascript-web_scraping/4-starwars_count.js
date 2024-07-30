c8b9726dad02db68eb3c69b03daaf31756678bec
edison asare <edisonpryce@gmail.com>
2024-07-30 15:08:19 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Request module make a GET request to the Star Wars
+ * API which returns with the list of films.
+ */
+const request = require('request');
+let bodyResponse;
+request.get(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.error('Error: ', err);
+    return;
+  }
+  bodyResponse = JSON.parse(body);
+  let counter = 0;
+  for (let i = 0; i < bodyResponse.results.length; i++) {
+    if (bodyResponse.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      counter += 1;
+    }
+  }
+  console.log(counter);
+});
